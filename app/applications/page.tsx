de58febedcac17cb5307f63bb7bad96006fbfba1
Sai Vimal <vimalsai2219@gmail.com>
2026-01-31 22:45:28 +0530

    Fix applications page default export

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -1,6 +1,9 @@
-import {
-  createApplication,
-  getApplications,
-  updateApplicationStatus,
-  deleteApplication
-} from '../lib/db/applications'
+'use client'
+
+export default function ApplicationsPage() {
+  return (
+    <div>
+      <h1>Applications Page</h1>
+    </div>
+  )
+}
